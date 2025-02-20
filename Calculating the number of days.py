18e9de12b6e66a3bfe184af7b222c383b57d0d6a
Eng-Moaz01 <mmezohrhera@gmail.com>
2025-02-20 19:33:54 +0200

    while with try

--- Calculating the number of days.py
+++ Calculating the number of days.py
@@ -1,7 +1,16 @@
 from datetime import datetime
-dob_str = input("Enter your birthdate (d,m,y) or 0 to close  -> ")     
-dob = datetime.strptime(dob_str, "%d/%m/%Y") 
-today = datetime.today()  
-difference = today - dob 
-days_lived = difference.days 
-print("Yoou have lived -> " , days_lived , "days")
+DAY = True
+while True:
+    try : 
+            dob_str = input("Enter your birthdate (d,m,y) or 0 to close  -> ")     
+            dob = datetime.strptime(dob_str, "%d/%m/%Y") 
+            today = datetime.today()  
+            difference = today - dob 
+            days_lived = difference.days 
+            days_lived = difference.days 
+            print("Yoou have lived -> " , days_lived , "days")
+    
+    except :
+        print("error")
+        DAY = False
+        break
