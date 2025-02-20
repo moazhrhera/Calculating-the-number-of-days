62452f6cffafc2d907467e6caac339b7db3723a9
Eng-Moaz01 <mmezohrhera@gmail.com>
2025-02-20 19:32:48 +0200

    Initial commit - Added calculating days script

---
+++ Calculating the number of days.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+dob_str = input("Enter your birthdate (d,m,y) or 0 to close  -> ")     
+dob = datetime.strptime(dob_str, "%d/%m/%Y") 
+today = datetime.today()  
+difference = today - dob 
+days_lived = difference.days 
+print("Yoou have lived -> " , days_lived , "days")
